94e183c65547058f509a720bd2f9ceb1b8d92dcb
lykovrs <lykovrs@gmail.com>
2017-12-18 16:46:03 +0300

    add leaflet

--- src/components/GlobalMap/index.js
+++ src/components/GlobalMap/index.js
@@ -1,35 +1,40 @@
-import React, { Component } from "react";
-import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import React from "react";
 import "./index.css";
+import { Map, TileLayer } from "react-leaflet";
+import MarkerClusterGroup from "react-leaflet-markercluster";
 
-class GlobalMap extends Component {
-  constructor() {
-    super();
-    this.state = {
-      lat: 51.505,
-      lng: -0.09,
-      zoom: 13
-    };
-  }
+export const MAP_CENTER_COORDINATES = [51.0, 19.0];
+export const MAP_ZOOM = 4;
+export const MAP_MAX_ZOOM = 18;
 
-  render() {
-    const position = [this.state.lat, this.state.lng];
-    return (
-      <Map center={position} zoom={this.state.zoom}>
-        <TileLayer
-          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={position}>
-          <Popup>
-            <span>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </span>
-          </Popup>
-        </Marker>
-      </Map>
-    );
-  }
-}
+const markers = [
+  { position: [50.4501, 30.5234] },
+  {
+    position: [52.2297, 21.0122],
+    options: { title: "Warszawa title on hover" }
+  },
+  { position: [50.0647, 19.945] },
+  {
+    position: [48.9226, 24.7111],
+    options: { title: "San Frankivsko title on hover" }
+  },
+  { position: [48.7164, 21.2611] }
+];
+
+const GlobalMap = () => (
+  <Map
+    className="markercluster-map"
+    center={MAP_CENTER_COORDINATES}
+    zoom={MAP_ZOOM}
+    maxZoom={MAP_MAX_ZOOM}
+  >
+    <TileLayer
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+    />
+
+    <MarkerClusterGroup markers={markers} />
+  </Map>
+);
 
 export default GlobalMap;
