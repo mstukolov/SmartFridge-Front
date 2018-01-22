03c0d8dd2a6a0728d99a7a08a59620d2f7d3015e
lykovrs <lykovrs@gmail.com>
2018-01-22 19:05:41 +0300

    change show form component

--- src/components/RetailEquipment/Form/index.js
+++ src/components/RetailEquipment/Form/index.js
@@ -5,6 +5,8 @@ import PropTypes from "prop-types";
 import { withStyles } from "material-ui/styles";
 import { connect } from "react-redux";
 import TextField from "material-ui/TextField";
+import FormControl from "material-ui/Form/FormControl";
+import FormHelperText from "material-ui/Form/FormHelperText";
 import Button from "material-ui/Button";
 import {
   cancelEquipment,
@@ -69,6 +71,16 @@ const styles = theme => ({
   },
   refillIconDown: {
     color: red[500]
+  },
+  text: {
+    display: "block",
+    marginBottom: "8px",
+    marginTop: "16px",
+    width: "100%",
+    "& > span": {
+      color: "#000",
+      padding: "6px 0"
+    }
   }
 });
 
@@ -244,7 +256,7 @@ class RetailMoreInfo extends React.Component {
    * @return {ReactElement} разметка React
    */
   render() {
-    const { classes } = this.props;
+    const { classes, edit } = this.props;
     return (
       <form
         onSubmit={this.handleSubmit}
@@ -257,110 +269,162 @@ class RetailMoreInfo extends React.Component {
         {this.showError()}
         {this.showSuccessSaved()}
 
-        <TextField
-          id="full-width"
-          label="Серийный номер"
-          onChange={this.handleChange("serialNumber")}
-          name={"serial"}
-          value={this.state.serialNumber}
-          disabled={this.isDisabledControl()}
-          InputLabelProps={{
-            shrink: true
-          }}
-          placeholder=""
-          helperText="15 символов"
-          fullWidth
-          margin="normal"
-        />
+        {edit ? (
+          <TextField
+            id="full-width"
+            label="Серийный номер"
+            onChange={this.handleChange("serialNumber")}
+            name={"serial"}
+            value={this.state.serialNumber}
+            disabled={this.isDisabledControl()}
+            InputLabelProps={{
+              shrink: true
+            }}
+            placeholder=""
+            helperText="15 символов"
+            fullWidth
+            margin="normal"
+          />
+        ) : (
+          <FormControl className={classes.text}>
+            <FormHelperText>Серийный номер</FormHelperText>
+            <span className={classes.text}>{this.state.serialNumber}</span>
+          </FormControl>
+        )}
 
-        <TextField
-          id="full-width"
-          label="Торговая сеть"
-          onChange={this.handleChange("commercialNetwork")}
-          name={"serial"}
-          value={this.state.commercialNetwork}
-          disabled={this.isDisabledControl()}
-          InputLabelProps={{
-            shrink: true
-          }}
-          placeholder=""
-          helperText="15 символов"
-          fullWidth
-          margin="normal"
-        />
+        {edit ? (
+          <TextField
+            id="full-width"
+            label="Торговая сеть"
+            onChange={this.handleChange("commercialNetwork")}
+            name={"serial"}
+            value={this.state.commercialNetwork}
+            disabled={this.isDisabledControl()}
+            InputLabelProps={{
+              shrink: true
+            }}
+            placeholder=""
+            helperText="15 символов"
+            fullWidth
+            margin="normal"
+          />
+        ) : (
+          <FormControl className={classes.text}>
+            <FormHelperText>Торговая сеть</FormHelperText>
+            <span className={classes.text}>{this.state.commercialNetwork}</span>
+          </FormControl>
+        )}
 
-        <TextField
-          id="full-width"
-          label="Торговая точка"
-          // onChange={this.handleChange("commercialNetwork")}
-          // name={"serial"}
-          value={this.state.tradePoint}
-          disabled={this.isDisabledControl()}
-          InputLabelProps={{
-            shrink: true
-          }}
-          placeholder=""
-          helperText="15 символов"
-          fullWidth
-          margin="normal"
-        />
+        {edit ? (
+          <TextField
+            id="full-width"
+            label="Торговая точка"
+            // onChange={this.handleChange("commercialNetwork")}
+            // name={"serial"}
+            value={this.state.tradePoint}
+            disabled={this.isDisabledControl()}
+            InputLabelProps={{
+              shrink: true
+            }}
+            placeholder=""
+            helperText="15 символов"
+            fullWidth
+            margin="normal"
+          />
+        ) : (
+          <FormControl className={classes.text}>
+            <FormHelperText>Торговая точка</FormHelperText>
+            <span className={classes.text}>{this.state.tradePoint}</span>
+          </FormControl>
+        )}
 
-        <TextField
-          id="full-width"
-          label="Максимальный вес"
-          value={this.state.remain}
-          onChange={this.handleChange("remain")}
-          disabled={this.isDisabledControl()}
-          InputLabelProps={{
-            shrink: true
-          }}
-          placeholder=""
-          helperText="%"
-          fullWidth
-          margin="normal"
-        />
-        <TextField
-          id="full-width"
-          label="Текущий вес"
-          value={this.state.remain}
-          onChange={this.handleChange("remain")}
-          disabled={this.isDisabledControl()}
-          InputLabelProps={{
-            shrink: true
-          }}
-          placeholder=""
-          helperText="%"
-          fullWidth
-          margin="normal"
-        />
+        {edit ? (
+          <TextField
+            id="full-width"
+            label="Максимальный вес"
+            value={this.state.remain}
+            onChange={this.handleChange("remain")}
+            disabled={this.isDisabledControl()}
+            InputLabelProps={{
+              shrink: true
+            }}
+            placeholder=""
+            helperText="%"
+            fullWidth
+            margin="normal"
+          />
+        ) : (
+          <FormControl className={classes.text}>
+            <FormHelperText>Максимальный вес</FormHelperText>
+            <span className={classes.text}>{this.state.remain}</span>
+          </FormControl>
+        )}
 
-        <TextField
-          id="full-width"
-          label="Процент наполнения"
-          value={this.state.remain}
-          onChange={this.handleChange("remain")}
-          disabled={this.isDisabledControl()}
-          InputLabelProps={{
-            shrink: true
-          }}
-          placeholder=""
-          helperText="%"
-          fullWidth
-          margin="normal"
-        />
+        {edit ? (
+          <TextField
+            id="full-width"
+            label="Текущий вес"
+            value={this.state.remain}
+            onChange={this.handleChange("remain")}
+            disabled={this.isDisabledControl()}
+            InputLabelProps={{
+              shrink: true
+            }}
+            placeholder=""
+            helperText="%"
+            fullWidth
+            margin="normal"
+          />
+        ) : (
+          <FormControl className={classes.text}>
+            <FormHelperText>Текущий вес</FormHelperText>
+            <span className={classes.text}>{this.state.remain}</span>
+          </FormControl>
+        )}
 
-        <TextField
-          id="datetime-local"
-          label="Последнее измерение"
-          type="datetime-local"
-          defaultValue={moment(this.state.dateUpdate).format(
-            "YYYY-MM-DDThh:mm"
-          )}
-          disabled={this.isDisabledControl()}
-          InputLabelProps={{
-            shrink: true
-          }}
-        />
+        {edit ? (
+          <TextField
+            id="full-width"
+            label="Процент наполнения"
+            value={this.state.remain}
+            onChange={this.handleChange("remain")}
+            disabled={this.isDisabledControl()}
+            InputLabelProps={{
+              shrink: true
+            }}
+            placeholder=""
+            helperText="%"
+            fullWidth
+            margin="normal"
+          />
+        ) : (
+          <FormControl className={classes.text}>
+            <FormHelperText>Процент наполнения</FormHelperText>
+            <span className={classes.text}>{this.state.remain}</span>
+          </FormControl>
+        )}
+
+        {edit ? (
+          <TextField
+            id="datetime-local"
+            label="Последнее измерение"
+            type="datetime-local"
+            defaultValue={moment(this.state.dateUpdate).format(
+              "YYYY-MM-DDThh:mm"
+            )}
+            disabled={this.isDisabledControl()}
+            InputLabelProps={{
+              shrink: true
+            }}
+          />
+        ) : (
+          <FormControl>
+            <FormHelperText>Последнее измерение</FormHelperText>
+            <span className={classes.text}>
+              {moment(this.state.dateUpdate).format("YYYY-MM-DDThh:mm")}
+            </span>
+          </FormControl>
+        )}
 
         {this.state.refill ? (
           <Tooltip
@@ -392,15 +456,22 @@ class RetailMoreInfo extends React.Component {
           </Tooltip>
         )}
 
-        <TextField
-          fullWidth
-          id="multiline-flexible"
-          label="Дополниельная информация"
-          disabled={this.isDisabledControl()}
-          multiline
-          rowsMax="4"
-          onChange={this.handleChange("additionalInformation")}
-        />
+        {edit ? (
+          <TextField
+            fullWidth
+            id="multiline-flexible"
+            label="Дополниельная информация"
+            disabled={this.isDisabledControl()}
+            multiline
+            rowsMax="4"
+            onChange={this.handleChange("additionalInformation")}
+          />
+        ) : (
+          <FormControl className={classes.text}>
+            <FormHelperText>Дополниельная информация</FormHelperText>
+            <span className={classes.text}>{}</span>
+          </FormControl>
+        )}
 
         {this.getButtonSet()}
       </form>
