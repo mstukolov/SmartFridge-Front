16c6db1a489af16df332120cbe9d42894425c7eb
Roman Lykov <lykovrs@gmail.com>
2018-02-13 17:43:20 +0300

    auth reducer tests

--- src/ducks/Auth/test.js
+++ src/ducks/Auth/test.js
@@ -5,8 +5,13 @@ import {
   authorizeAction,
   AUTH_REQUEST,
   logOutAction,
-  LOG_OUT_REQUEST
+  LOG_OUT_REQUEST,
+  AUTH_SUCCESS,
+  AUTH_FAILURE,
+  LOG_OUT_SUCCESS
 } from "./index";
+
+import reducer from "./index";
 
 const state = new ReducerRecord();
 
@@ -31,6 +36,10 @@ const loginPayload = {
   password: 123
 };
 
+const errorPayload = {
+  message: "Ошибка авторизации"
+};
+
 describe("Auth actions", () => {
   describe("authorizeAction", () => {
     it("should create an action to log in", () => {
@@ -51,5 +60,40 @@ describe("Auth actions", () => {
       };
       expect(logOutAction()).toEqual(expectedAction);
     });
+  });
+});
+
+describe("Auth reducer", () => {
+  it("should return the initial state", () => {
+    expect(reducer(undefined, {})).toEqual(new ReducerRecord());
+  });
+
+  it("should handle AUTH_SUCCESS", () => {
+    const successAction = {
+      type: AUTH_SUCCESS,
+      payload: loginPayload // important to pass correct payload, that's what the tests are for ;)
+    };
+
+    const state = new ReducerRecord().setIn(["token"], loginPayload);
+    expect(reducer(undefined, successAction)).toEqual(state);
+  });
+
+  it("should handle AUTH_FAILURE", () => {
+    const errorAction = {
+      type: AUTH_FAILURE,
+      payload: errorPayload // important to pass correct payload, that's what the tests are for ;)
+    };
+
+    const state = new ReducerRecord().setIn(["error"], errorPayload);
+    expect(reducer(undefined, errorAction)).toEqual(state);
+  });
+
+  it("should handle LOG_OUT_SUCCESS", () => {
+    const logoutAction = {
+      type: LOG_OUT_SUCCESS
+    };
+
+    const logInState = new ReducerRecord().setIn(["token"], loginPayload);
+    expect(reducer(logInState, logoutAction)).toEqual(new ReducerRecord());
   });
 });
